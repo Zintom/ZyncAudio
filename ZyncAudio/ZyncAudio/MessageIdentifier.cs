bcb9149d1f7987eed51e2fa89468326b098d1bd9
Zintom <tzsinkoapps@gmail.com>
2021-04-17 21:49:07 +0100

    Create MessageIdentifier.cs

---
+++ ZyncAudio/ZyncAudio/MessageIdentifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZyncAudio
+{
+    [Flags]
+    public enum MessageIdentifier
+    {
+        None = 0,
+        Request = 1,
+        Response = 2,
+        WaveFormatInformation = 4,
+        AudioSamples = 8
+    }
+}
