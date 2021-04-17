1f0b069dadd860606c48290c220596bdff450035
Zintom <tzsinkoapps@gmail.com>
2021-04-17 21:49:05 +0100

    Create ILogger.cs

---
+++ ZyncAudio/ZyncAudio/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace ZyncAudio
+{
+    public interface ILogger
+    {
+        void Log(string message, LogLevel severity = LogLevel.Information);
+
+        public enum LogLevel
+        {
+            Information,
+            Warning,
+            Error
+        }
+    }
+}
