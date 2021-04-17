6e6f2b04e64953a276100cf2cdac7d2277a65147
Zintom <tzsinkoapps@gmail.com>
2021-04-17 21:48:58 +0100

    Create WaveFormatHelper.cs
    Provides helper methods for the WaveFormat object.

---
+++ ZyncAudio/ZyncAudio/Extensions/WaveFormatHelper.cs
@@ -0,0 +1,37 @@
+﻿using NAudio.Wave;
+using System.IO;
+
+namespace ZyncAudio.Extensions
+{
+    public static class WaveFormatHelper
+    {
+
+        /// <summary>
+        /// Gets the bitrate for the given <see cref="WaveFormat"/>.
+        /// </summary>
+        public static int GetBitrate(this WaveFormat waveFormat)
+        {
+            return waveFormat.SampleRate * waveFormat.BitsPerSample * waveFormat.Channels;
+        }
+
+        public static byte[] ToBytes(WaveFormat waveFormat)
+        {
+            using (MemoryStream formatStream = new MemoryStream())
+            using (var writer = new BinaryWriter(formatStream))
+            {
+                waveFormat.Serialize(writer);
+                return formatStream.ToArray();
+            }
+        }
+
+        public static WaveFormat FromBytes(byte[] bytes)
+        {
+            using (var stream = new MemoryStream(bytes))
+            using (var binaryReader = new BinaryReader(stream))
+            {
+                return new WaveFormat(binaryReader);
+            }
+        }
+
+    }
+}
