3ea71ccd3c6deba6f8e5c27c69717ecdbb79ea96
Zintom <tzsinkoapps@gmail.com>
2021-04-17 21:47:14 +0100

    Added Socket Server/Client
    Handles the underlying connection between a host and clients.

---
+++ ZyncAudio/ZyncAudio/Client/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ZyncAudio.Extensions;
+
+namespace ZyncAudio
+{
+    public interface ISocketClient
+    {
+        Action<byte[], Socket>? DataReceived { get; set; }
+
+        Action<SocketException, Socket>? SocketError { get; set; }
+
+        void Connect(IPAddress address, int port);
+
+        void Send(byte[] data);
+
+        void Send<T>(byte[] data, TaskCompletionSource<T>? taskCompletionSource);
+    }
+
+    /// <summary>
+    /// A basic client which connects asynchronously.
+    /// </summary>
+    /// <remarks>Send will use the length-prefixed option.
+    /// <para/>
+    /// Receive will begin looping once connected.
+    /// </remarks>
+    public sealed class Client : ISocketClient
+    {
+        private readonly Socket _workerSocket;
+
+        public Action<byte[], Socket>? DataReceived { get; set; }
+
+        public Action<SocketException, Socket>? SocketError { get; set; }
+
+        public Client()
+        {
+            _workerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _workerSocket.ReceiveTimeout = 5000;
+        }
+
+        /// <summary>
+        /// Connects to the remote <paramref name="address"/> on the given <paramref name="port"/>
+        /// and begins a receive loop once connection is established.
+        /// </summary>
+        public void Connect(IPAddress address, int port)
+        {
+            _workerSocket.BeginConnect(new IPEndPoint(address, port), EndConnect, null);
+        }
+
+        private void EndConnect(IAsyncResult ar)
+        {
+            _workerSocket.EndConnect(ar);
+
+            // Begin receive loop
+            _workerSocket.BeginReceiveLengthPrefixed(true, DataReceived, SocketError);
+        }
+
+        public void Send(byte[] data)
+        {
+            Send<object>(data, null);
+        }
+
+        public void Send<T>(byte[] data, TaskCompletionSource<T>? taskCompletionSource)
+        {
+            _workerSocket.SendLengthPrefixed(data, SocketError, taskCompletionSource);
+        }
+    }
+}
---
+++ ZyncAudio/ZyncAudio/Host/Server.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using ZyncAudio.Extensions;
+
+namespace ZyncAudio
+{
+    public interface ISocketServer
+    {
+        Action<byte[], Socket>? DataReceived { get; set; }
+
+        Action<SocketException, Socket>? SocketError { get; set; }
+
+        Action? ClientConnected { get; set; }
+
+        void Open(IPAddress address, int port);
+
+        void Send(byte[] data, Socket client);
+
+        void SendAll(byte[] data);
+    }
+
+    /// <summary>
+    /// A basic multi-client socket server.
+    /// </summary>
+    /// <remarks>A receive loop is started for each client that connects.</remarks>
+    public sealed class Server : ISocketServer
+    {
+        private readonly Socket _listener;
+
+        private readonly List<Socket> _clients;
+
+        private readonly ILogger? _logger;
+
+        public Action<byte[], Socket>? DataReceived { get; set; }
+
+        public Action<SocketException, Socket>? SocketError { get; set; }
+
+        public Action? ClientConnected { get; set; }
+
+        public Server(ILogger? logger)
+        {
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _clients = new List<Socket>();
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Opens the server on the given <paramref name="address"/> on the given <paramref name="port"/>.
+        /// </summary>
+        public void Open(IPAddress address, int port)
+        {
+            _listener.Bind(new IPEndPoint(address, port));
+            _listener.Listen();
+
+            _logger?.Log($"Server open at {address} on port {port}");
+
+            _listener.BeginAccept(EndAccept, null);
+        }
+
+        private void EndAccept(IAsyncResult ar)
+        {
+            Socket client = _listener.EndAccept(ar);
+
+            _logger?.Log($"Client connected ({client.RemoteEndPoint})");
+            ClientConnected?.Invoke();
+
+            _clients.Add(client);
+
+            client.BeginReceiveLengthPrefixed(
+                true,
+                (d, s) => { DataReceived?.Invoke(d, s); },
+                HandleSocketException);
+
+            // Begin accepting other clients
+            _listener.BeginAccept(EndAccept, null);
+        }
+
+        public void Send(byte[] data, Socket client)
+        {
+            client.SendLengthPrefixed(data, HandleSocketException);
+        }
+
+        public void SendAll(byte[] data)
+        {
+            for (int i = 0; i < _clients.Count; i++)
+            {
+                _clients[i].SendLengthPrefixed(data, HandleSocketException);
+            }
+        }
+
+        private void HandleSocketException(SocketException e, Socket client)
+        {
+            _clients.Remove(client);
+            _logger?.Log($"Client forcefully disconnected ({client.RemoteEndPoint})");
+
+            SocketError?.Invoke(e, client);
+        }
+    }
+}
