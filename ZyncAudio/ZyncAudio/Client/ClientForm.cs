c3f03049be2ec31e60c092908bee7979e50388fd
Zintom <tzsinkoapps@gmail.com>
2021-04-17 21:48:32 +0100

    Initial GUI commit

---
+++ ZyncAudio/ZyncAudio/Client/ClientForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZyncAudio
+{
+    public partial class ClientForm : Form
+    {
+        Client _client;
+        AudioClient _audioClient;
+
+        ILogger _logger;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            HostForm host = new HostForm();
+            host.Show();
+
+            _logger = new ConsoleLogger();
+            _client = new Client();
+            _audioClient = new AudioClient(_client, _logger);
+
+            _client.Connect(IPAddress.Loopback, 60759);
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ ZyncAudio/ZyncAudio/Host/HostForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZyncAudio
+{
+    public partial class HostForm : Form
+    {
+        private Server _server;
+        private AudioServer _audioServer;
+
+        private ILogger _logger;
+
+        public HostForm()
+        {
+            InitializeComponent();
+
+            _logger = new ConsoleLogger();
+            _server = new Server(_logger);
+            _audioServer = new AudioServer(_server);
+
+            _server.Open(IPAddress.Any, 60759);
+            _server.ClientConnected = ClientConnected;
+        }
+
+        private void ClientConnected()
+        {
+            _audioServer.PlaySong(@"D:\All Files\Music\04. Mike Ault - Flying Forever (feat. Morgan Perry).flac");
+        }
+
+        private void HostForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class FormLogger : ILogger
+    {
+        public void Log(string message, ILogger.LogLevel severity = ILogger.LogLevel.Information)
+        {
+            MessageBox.Show(text: message, caption: severity.ToString());
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message, ILogger.LogLevel severity = ILogger.LogLevel.Information)
+        {
+            Debug.WriteLine($"{severity}: {message}");
+        }
+    }
+}
