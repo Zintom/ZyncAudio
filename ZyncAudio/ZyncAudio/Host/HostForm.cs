20aface493fc3599f49c6b3a287a46f8c7101dc1
Zintom <tzsinkoapps@gmail.com>
2021-04-18 15:24:34 +0100

    Update HostForm.cs
    Improved UI.

--- ZyncAudio/ZyncAudio/Host/HostForm.cs
+++ ZyncAudio/ZyncAudio/Host/HostForm.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,22 +24,93 @@ namespace ZyncAudio
         {
             InitializeComponent();
 
+            // Init list view
+            ClientListView.View = View.Details;
+            ClientListView.FullRowSelect = true;
+            ClientListView.MultiSelect = false;
+            ClientListView.Columns.Add("address", "Address", 120);
+            ClientListView.Columns.Add("ping", "Ping", 50);
+
             _logger = new ConsoleLogger();
             _server = new Server(_logger);
             _audioServer = new AudioServer(_server);
 
             _server.Open(IPAddress.Any, 60759);
             _server.ClientConnected = ClientConnected;
+            _server.ClientDisconnected = ClientDisconnected;
         }
 
-        private void ClientConnected()
+        private void ClientConnected(Socket client)
         {
-            _audioServer.PlaySong(@"D:\All Files\Music\04. Mike Ault - Flying Forever (feat. Morgan Perry).flac");
+            Invoke(new Action(() =>
+            {
+                CloseEntryBtn.Enabled = true;
+
+                ClientListView.Items.Add(new ListViewItem(new string[] { client.RemoteEndPoint?.ToString() ?? "null", "0ms" }));
+            }));
+        }
+
+        private void ClientDisconnected(Socket client)
+        {
+            Invoke(new Action(() =>
+            {
+                string remoteAddress = client.RemoteEndPoint?.ToString() ?? "null";
+                foreach(ListViewItem item in ClientListView.Items)
+                {
+                    if(item.Text == remoteAddress)
+                    {
+                        ClientListView.Items.Remove(item);
+                        break;
+                    }
+                }
+            }));
         }
 
         private void HostForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            _audioServer.Play(@"D:\All Files\Music\04. Mike Ault - Flying Forever (feat. Morgan Perry).flac");
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            _audioServer.Stop();
+        }
+
+        private void CloseEntryBtn_Click(object sender, EventArgs e)
+        {
+            _server.StopAccepting();
+            CloseEntryBtn.Enabled = false;
+
+            PlayBtn.Enabled = true;
+            StopBtn.Enabled = true;
+        }
+
+        private void PingChecker_Tick(object sender, EventArgs e)
+        {
+            foreach(var pair in _audioServer.Pinger.PingStatistics)
+            {
+                string ping = pair.Value + "ms";
+                string address = pair.Key.RemoteEndPoint?.ToString() ?? "null";
+
+                foreach(ListViewItem item in ClientListView.Items)
+                {
+                    if(item.Text == address)
+                    {
+                        item.SubItems[1].Text = ping;
+                    }
+                }
+            }
+        }
+
+        private void HostForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _audioServer.Stop();
+            _server.Close();
         }
     }
 
