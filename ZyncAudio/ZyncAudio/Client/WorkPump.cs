c7a77ca6cdc9d0322679d8ef208c433deac6f809
Zintom <tzsinkoapps@gmail.com>
2021-04-21 22:42:08 +0100

    Create WorkPump.cs
    The WorkPump class handles a work queue on a dedicated thread, the specification of that thread can be customized by the caller when `Run` is called, such as the ThreadPriority and the Name.
    
    The work loop uses an efficient WaitHandle system so that the thread doesn't uselessly spin when waiting for work.
    
    Defines the IWorkItem which callers can implement to pass their work to the pump, their work must initiate on a call to `Invoke()`.

---
+++ ZyncAudio/ZyncAudio/Client/WorkPump.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ZyncAudio
+{
+    public class WorkPump
+    {
+        private readonly ConcurrentQueue<IWorkItem> _workQueue = new();
+
+        private readonly object _runLocker = new();
+
+        private readonly object _workQueueFinishedLocker = new();
+
+        private readonly ManualResetEvent _waitForWorkSignal = new(false);
+
+        private volatile bool _stopPump;
+
+        /// <summary>
+        /// Runs the work pump on a new thread with the given <paramref name="threadPriority"/>.
+        /// </summary>
+        public void Run(ThreadPriority threadPriority, string threadName = "WorkPumpThread")
+        {
+            new Thread(RunInternal)
+            {
+                Priority = threadPriority,
+                Name = threadName
+            }.Start();
+        }
+
+        private void RunInternal()
+        {
+            if (!Monitor.TryEnter(_runLocker)) throw new InvalidOperationException("The message pump cannot be ran twice at the same time!");
+
+            while (!_stopPump)
+            {
+                while (_workQueue.TryDequeue(out IWorkItem? work) && !_stopPump)
+                {
+                    work.Invoke();
+                }
+
+                lock (_workQueueFinishedLocker)
+                {
+                    // If the work queue is confirmed as Empty, then halt the loop until work becomes available.
+                    if (_workQueue.IsEmpty)
+                    {
+                        _waitForWorkSignal.Reset();
+                    }
+                }
+
+                // Halt the loop until work becomes available.
+                _waitForWorkSignal.WaitOne();
+            }
+
+            _stopPump = false;
+            Monitor.Exit(_runLocker);
+        }
+
+        public void Add(IWorkItem workItem)
+        {
+            lock (_workQueueFinishedLocker)
+            {
+                _workQueue.Enqueue(workItem);
+
+                // Inform loop that the work queue is not empty.
+                _waitForWorkSignal.Set();
+            }
+        }
+
+        /// <summary>
+        /// Stops the pump from processing further work items, does not cancel the currently running work.
+        /// </summary>
+        public void Stop()
+        {
+            lock (_workQueueFinishedLocker)
+            {
+                _stopPump = true;
+                _waitForWorkSignal.Set();
+            }
+        }
+
+        /// <summary>
+        /// Represents invokable work that does not return a value.
+        /// </summary>
+        public interface IWorkItem
+        {
+            void Invoke();
+        }
+    }
+}
