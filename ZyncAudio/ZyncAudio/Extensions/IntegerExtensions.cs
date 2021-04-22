48aeaec0a86895ecbba68622df1128cb334d192b
Zintom <tzsinkoapps@gmail.com>
2021-04-22 12:09:32 +0100

    Create IntegerExtensions.cs
    Added the Modulo function; this implementation differs from using the C# `%` (Remainder) operator as this implementation works on negative numbers too.

---
+++ ZyncAudio/ZyncAudio/Extensions/IntegerExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ZyncAudio
+{
+    public static class IntegerExtensions
+    {
+
+        /// <summary>
+        /// Computes the Modulo for the given <paramref name="dividend"/> and <paramref name="divisor"/>.
+        /// </summary>
+        /// <remarks>This implementation differs from using the C# `%` (Remainder) operator as this implementation works on negative numbers too.</remarks>
+        public static int Mod(this int dividend, int divisor)
+        {
+            return dividend < 0 ? ((dividend + 1) % divisor + divisor - 1) : (dividend % divisor);
+        }
+
+    }
+}
