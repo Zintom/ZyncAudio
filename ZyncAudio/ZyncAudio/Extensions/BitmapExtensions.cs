813fce09af79db47e64296ef5f1a0d51cb2c83d0
Zintom <tzsinkoapps@gmail.com>
2021-04-24 00:42:54 +0100

    Create TintableButton
    Custom control TintableButton fills in missing functionality from the original Button class. You can now specify a tint which will be applied to the BackgroundImage of the button.
    
    I have also added a Checked state and a Checked Tint colour. Although this does not technically fall under the remit of the TintableButton it is needed in the GUI.

---
+++ ZyncAudio/ZyncAudio/Extensions/BitmapExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace ZyncAudio.Extensions
+{
+    public static class BitmapExtensions
+    {
+
+        public static void Tint(this Bitmap bitmap, Color newColor)
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    if (pixelColor.A != 0)
+                    {
+                        bitmap.SetPixel(x, y, newColor);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ ZyncAudio/ZyncAudio/TintableButton.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using ZyncAudio.Extensions;
+
+namespace ZyncAudio
+{
+    public class TintableButton : Button
+    {
+        public override Image BackgroundImage
+        {
+            get => base.BackgroundImage;
+            set
+            {
+                base.BackgroundImage = value;
+                ApplyTint();
+            }
+        }
+
+        private bool _checked;
+        public bool Checked
+        {
+            get => _checked;
+            set
+            {
+                _checked = value;
+                ApplyTint();
+            }
+        }
+
+        private Color _checkedTint = Color.Empty;
+        public Color CheckedTint
+        {
+            get => _checkedTint;
+            set
+            {
+                _checkedTint = value;
+                ApplyTint();
+            }
+        }
+
+        private Color _tint = Color.Empty;
+        public Color Tint
+        {
+            get => _tint;
+            set
+            {
+                _tint = value;
+                ApplyTint();
+            }
+        }
+
+        private RotateFlipType _flipEffect = RotateFlipType.RotateNoneFlipNone;
+        public RotateFlipType FlipEffect
+        {
+            get => _flipEffect;
+            set
+            {
+                Bitmap flipped = (Bitmap)base.BackgroundImage;
+                flipped.RotateFlip(value);
+                _flipEffect = value;
+            }
+        }
+
+        /// <summary>
+        /// Applies the correct tint to the <see cref="BackgroundImage"/> depending on the <see cref="Checked"/> state.
+        /// </summary>
+        private void ApplyTint()
+        {
+            if (Checked)
+            {
+                if (CheckedTint == Color.Empty) return;
+                ((Bitmap)base.BackgroundImage).Tint(CheckedTint);
+            }
+            else
+            {
+                if (Tint == Color.Empty) return;
+                ((Bitmap)base.BackgroundImage).Tint(Tint);
+            }
+
+            Invalidate();
+        }
+
+    }
+}
