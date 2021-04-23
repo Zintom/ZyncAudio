7692c6600284da8666b41236a320e93fe8af5043
Zintom <tzsinkoapps@gmail.com>
2021-04-24 00:43:18 +0100

    Create IPAddressTextBox.cs
    Uses the built in system IP address box.

---
+++ ZyncAudio/ZyncAudio/IPAddressTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace ZyncAudio
+{
+    public class IPAddressTextBox : TextBox
+    {
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassName = "SysIPAddress32";
+                return cp;
+            }
+        }
+
+    }
+}
