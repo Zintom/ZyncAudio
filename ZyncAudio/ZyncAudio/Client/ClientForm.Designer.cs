6e7814ea42cb6aaba94840eb582e783197407fa8
Zintom <tzsinkoapps@gmail.com>
2021-04-24 00:40:49 +0100

    Update ClientForm.cs
    Added now playing scrolling text.
    
    Added a proper IP address text box rather than a generic text box.

--- ZyncAudio/ZyncAudio/Client/ClientForm.Designer.cs
+++ ZyncAudio/ZyncAudio/Client/ClientForm.Designer.cs
@@ -29,62 +29,109 @@ namespace ZyncAudio
         /// </summary>
         private void InitializeComponent()
         {
-            this.HostBtn = new System.Windows.Forms.Button();
-            this.ConnectBtn = new System.Windows.Forms.Button();
-            this.IPAddressInputBox = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this._hostBtn = new System.Windows.Forms.Button();
+            this._connectBtn = new System.Windows.Forms.Button();
+            this._ipAddressInputBox = new ZyncAudio.IPAddressTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._nowPlayingBarText = new System.Windows.Forms.Label();
+            this._nowPlayingBarAnimator = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // HostBtn
+            // _hostBtn
             // 
-            this.HostBtn.Location = new System.Drawing.Point(12, 12);
-            this.HostBtn.Name = "HostBtn";
-            this.HostBtn.Size = new System.Drawing.Size(97, 37);
-            this.HostBtn.TabIndex = 1;
-            this.HostBtn.Text = "Host Music";
-            this.HostBtn.UseVisualStyleBackColor = true;
-            this.HostBtn.Click += new System.EventHandler(this.HostButton_Click);
+            this._hostBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this._hostBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this._hostBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._hostBtn.Font = new System.Drawing.Font("Segoe UI Emoji", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this._hostBtn.ForeColor = System.Drawing.Color.White;
+            this._hostBtn.Location = new System.Drawing.Point(12, 12);
+            this._hostBtn.Name = "_hostBtn";
+            this._hostBtn.Size = new System.Drawing.Size(49, 49);
+            this._hostBtn.TabIndex = 1;
+            this._hostBtn.Text = "🌐";
+            this._hostBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this._hostBtn.UseVisualStyleBackColor = false;
+            this._hostBtn.Click += new System.EventHandler(this.HostButton_Click);
             // 
-            // ConnectBtn
+            // _connectBtn
             // 
-            this.ConnectBtn.Location = new System.Drawing.Point(12, 276);
-            this.ConnectBtn.Name = "ConnectBtn";
-            this.ConnectBtn.Size = new System.Drawing.Size(97, 37);
-            this.ConnectBtn.TabIndex = 2;
-            this.ConnectBtn.Text = "Connect";
-            this.ConnectBtn.UseVisualStyleBackColor = true;
-            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            this._connectBtn.Location = new System.Drawing.Point(12, 76);
+            this._connectBtn.Name = "_connectBtn";
+            this._connectBtn.Size = new System.Drawing.Size(97, 37);
+            this._connectBtn.TabIndex = 2;
+            this._connectBtn.Text = "Connect";
+            this._connectBtn.UseVisualStyleBackColor = true;
+            this._connectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
             // 
-            // IPAddressInputBox
+            // _ipAddressInputBox
             // 
-            this.IPAddressInputBox.Location = new System.Drawing.Point(115, 284);
-            this.IPAddressInputBox.Name = "IPAddressInputBox";
-            this.IPAddressInputBox.Size = new System.Drawing.Size(146, 23);
-            this.IPAddressInputBox.TabIndex = 3;
-            this.IPAddressInputBox.Text = "127.0.0.1";
+            this._ipAddressInputBox.Location = new System.Drawing.Point(115, 84);
+            this._ipAddressInputBox.Name = "_ipAddressInputBox";
+            this._ipAddressInputBox.Size = new System.Drawing.Size(135, 23);
+            this._ipAddressInputBox.TabIndex = 4;
+            this._ipAddressInputBox.Text = "255.255.255.255";
+            this._ipAddressInputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this._ipAddressInputBox.TextChanged += new System.EventHandler(this.IPAddressInputBox_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(100)))), ((int)(((byte)(190)))));
+            this.panel1.Controls.Add(this._nowPlayingBarText);
+            this.panel1.Location = new System.Drawing.Point(0, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 24);
+            this.panel1.TabIndex = 5;
+            // 
+            // _nowPlayingBarText
+            // 
+            this._nowPlayingBarText.AutoSize = true;
+            this._nowPlayingBarText.ForeColor = System.Drawing.Color.White;
+            this._nowPlayingBarText.Location = new System.Drawing.Point(3, 5);
+            this._nowPlayingBarText.Name = "_nowPlayingBarText";
+            this._nowPlayingBarText.Size = new System.Drawing.Size(0, 15);
+            this._nowPlayingBarText.TabIndex = 6;
+            // 
+            // _nowPlayingBarAnimator
+            // 
+            this._nowPlayingBarAnimator.Enabled = true;
+            this._nowPlayingBarAnimator.Interval = 32;
+            this._nowPlayingBarAnimator.Tick += new System.EventHandler(this.NowPlayingBarTick);
             // 
             // ClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 321);
-            this.Controls.Add(this.IPAddressInputBox);
-            this.Controls.Add(this.ConnectBtn);
-            this.Controls.Add(this.HostBtn);
+            this.ClientSize = new System.Drawing.Size(344, 177);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this._ipAddressInputBox);
+            this.Controls.Add(this._connectBtn);
+            this.Controls.Add(this._hostBtn);
             this.Name = "ClientForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Zync Audio Client";
             this.Activated += new System.EventHandler(this.ClientForm_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
             this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button HostBtn;
-        private System.Windows.Forms.Button ConnectBtn;
-        private System.Windows.Forms.TextBox IPAddressInputBox;
+        private System.Windows.Forms.Button _hostBtn;
+        private System.Windows.Forms.Button _connectBtn;
+        private IPAddressTextBox ipAddressTextBox1;
+        private IPAddressTextBox _ipAddressInputBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label _nowPlayingBarText;
+        private System.Windows.Forms.Timer _nowPlayingBarAnimator;
     }
 }
 
--- ZyncAudio/ZyncAudio/Client/ClientForm.cs
+++ ZyncAudio/ZyncAudio/Client/ClientForm.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Zintom.StorageFacility;
 
 namespace ZyncAudio
 {
@@ -24,11 +18,15 @@ namespace ZyncAudio
         public ClientForm()
         {
             InitializeComponent();
+            _nowPlayingBarText.Text = Program.NoAudioPlaying;
 
             _logger = new ConsoleLogger();
             _client = new Client();
             _client.SocketError = HandleSocketError;
             _audioClient = new AudioClient(_client, _logger);
+            _audioClient.TrackInformationChanged += UpdateTrackInformationText;
+
+            _ipAddressInputBox.Text = Storage.GetStorage(Program.SettingsFile).Strings.GetValue("lastConnectedIpAddress", "127.0.0.1");
         }
 
         private void ClientForm_Load(object sender, EventArgs e)
@@ -37,7 +35,7 @@ namespace ZyncAudio
 
         private void HostButton_Click(object sender, EventArgs e)
         {
-            HostBtn.Enabled = false;
+            _hostBtn.Enabled = false;
 
             _host = new HostForm();
             _host.Show();
@@ -61,17 +59,69 @@ namespace ZyncAudio
                 if (_client.Disconnect())
                 {
                     _connectToggle = !_connectToggle;
-                    ConnectBtn.Text = "Connect";
+                    _connectBtn.Text = "Connect";
                 }
+
+                _audioClient.Disconnect();
             }
             else
             {
-                if (_client.Connect(IPAddress.Parse(IPAddressInputBox.Text), 60759))
+                if (_client.Connect(IPAddress.Parse(_ipAddressInputBox.Text), 60759))
                 {
                     _connectToggle = !_connectToggle;
-                    ConnectBtn.Text = "Disconnect";
+                    _connectBtn.Text = "Disconnect";
                 }
             }
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _audioClient.Disconnect();
+            Storage.GetStorage(Program.SettingsFile).Edit().Commit();
+        }
+
+        private void IPAddressInputBox_TextChanged(object sender, EventArgs e)
+        {
+            Storage.GetStorage(Program.SettingsFile).Edit().PutValue("lastConnectedIpAddress", _ipAddressInputBox.Text);
+        }
+
+        private void UpdateTrackInformationText(string nowPlaying)
+        {
+            _nowPlayingBarText.Invoke(new Action(() =>
+            {
+                if (nowPlaying == Program.NoAudioPlaying)
+                {
+                    // Lock the now playing text to the left of the bar
+                    _nowPlayingBarText.Left = 3;
+                    _nowPlayingBarAnimator.Stop();
+                }
+                else
+                {
+                    // Place the now playing text a little bit inwards from the left
+                    // so that the user can see the start of the text before it animates off screen.
+                    _nowPlayingBarText.Left = 40;
+                    _nowPlayingBarAnimator.Start();
+                }
+
+                _nowPlayingBarText.Text = nowPlaying;
+            }));
+        }
+
+        private void NowPlayingBarTick(object sender, EventArgs e)
+        {
+            if (_nowPlayingBarText.Right < 0)
+            {
+                _nowPlayingBarText.Left = _nowPlayingBarText.Parent.Width;
+            }
+
+            if (_nowPlayingBarText.Left > _nowPlayingBarText.Parent.Width)
+            {
+                _nowPlayingBarText.Left = _nowPlayingBarText.Parent.Width;
+            }
+
+            //_nowPlayingBarText.Left = Math.Clamp(_nowPlayingBarText.Left, 0 - _nowPlayingBarText.Width, _nowPlayingBarText.Parent.Width);
+
+            _nowPlayingBarText.Left -= 2;
         }
     }
 }
