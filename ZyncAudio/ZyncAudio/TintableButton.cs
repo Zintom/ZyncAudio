0a217f74381547e232496f5d3288c3cd1a61a2ed
Zintom <tzsinkoapps@gmail.com>
2021-04-30 10:39:43 +0100

    Update TintableButton.cs
    The control now tints itself gray when in the Disabled state.

--- ZyncAudio/ZyncAudio/TintableButton.cs
+++ ZyncAudio/ZyncAudio/TintableButton.cs
@@ -1,18 +1,48 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using ZyncAudio.Extensions;
 
 namespace ZyncAudio
 {
-    public class TintableButton : Button
+    public class TintableButton : Button, IDisposable
     {
+        /// <summary>
+        /// This is so that we may restore an untinted image if the control goes from
+        /// being Disabled > Enabled and NO tint or checked tint is applied.
+        /// </summary>
+        private Bitmap? _originalImage;
+
         public override Image BackgroundImage
         {
-            get => base.BackgroundImage;
+            get
+            {
+                if (UseOriginalBackgroundImage && _originalImage != null)
+                {
+                    return _originalImage;
+                }
+                else
+                {
+                    return base.BackgroundImage;
+                }
+            }
             set
             {
+                _originalImage = (Bitmap)value.Clone();
                 base.BackgroundImage = value;
                 ApplyTint();
+            }
+        }
+
+        /// <summary>
+        /// The only time we should the original background image
+        /// is if the control isn't <b>Disabled</b> (otherwise it would be grey tint) and has <i>NO</i> <b>Tint</b> or <b>Checked Tint</b> for its enabled state.
+        /// </summary>
+        private bool UseOriginalBackgroundImage
+        {
+            get
+            {
+                return (Checked && CheckedTint == Color.Empty) || (!Checked && Tint == Color.Empty) && Enabled;
             }
         }
 
@@ -50,6 +80,8 @@ namespace ZyncAudio
         }
 
         private RotateFlipType _flipEffect = RotateFlipType.RotateNoneFlipNone;
+        private bool _disposedValue;
+
         public RotateFlipType FlipEffect
         {
             get => _flipEffect;
@@ -61,12 +93,22 @@ namespace ZyncAudio
             }
         }
 
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            ApplyTint();
+            base.OnEnabledChanged(e);
+        }
+
         /// <summary>
         /// Applies the correct tint to the <see cref="BackgroundImage"/> depending on the <see cref="Checked"/> state.
         /// </summary>
         private void ApplyTint()
         {
-            if (Checked)
+            if (!Enabled)
+            {
+                ((Bitmap)base.BackgroundImage).Tint(Color.Gray);
+            }
+            else if (Checked)
             {
                 if (CheckedTint == Color.Empty) return;
                 ((Bitmap)base.BackgroundImage).Tint(CheckedTint);
@@ -78,6 +120,25 @@ namespace ZyncAudio
             }
 
             Invalidate();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    _originalImage?.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _originalImage = null;
+                _disposedValue = true;
+            }
+
+            base.Dispose(disposing);
         }
 
     }
